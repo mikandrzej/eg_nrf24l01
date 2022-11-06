efb2fcbc9da274ad33cab4645ee7b5a2e04ea366
Mikolaj Andrzejewski <mikandrzej12321@gmail.com>
2022-11-06 22:49:38 +0100

    Added state machine state handlers and some features. Live 6.11.2022

--- eg_nrf24l01.c
+++ eg_nrf24l01.c
@@ -8,7 +8,52 @@
 #define EG_NRF24L01_SETUP_AW_4BYTES 2u
 #define EG_NRF24L01_SETUP_AW_5BYTES 3u
 
-eg_nrf_error_e eg_nrf24l01_config(eg_nrf24l01_state_s *state, eg_nrf24l01_init_data_s *init_data)
+#define POWER_ON_TIME_MS 100u /**< Time needed to power on then module */
+
+#define NRF_CMD_READ_REG 0x00u
+#define NRF_CMD_WRITE_REG 0x20u
+#define NRF_CMD_NOP 0xFFu
+#define NRF_CMD_W_TX_PAYLOAD 0xA0u
+#define NRF_CMD_W_TX_PAYLOAD_NO_ACK 0xB0u
+#define NRF_CMD_FLUSH_TX 0xE1
+#define NRF_CMD_FLUSH_RX 0xE2
+
+#define NRF_REG_TX_ADDR 0x10u
+#define NRF_REG_RX_ADDR_P0 0x0Au
+#define NRF_REG_CONFIG 0x00u
+
+static void sm_state_power_off_handler(eg_nrf24l01_state_s *state);
+static void sm_state_module_startup_handler(eg_nrf24l01_state_s *state);
+static void sm_state_configure_handler(eg_nrf24l01_state_s *state);
+static void sm_state_sleep_handler(eg_nrf24l01_state_s *state);
+static void sm_state_idle_handler(eg_nrf24l01_state_s *state);
+static void sm_state_ext_int_handler(eg_nrf24l01_state_s *state);
+static void sm_state_status_reading_handler(eg_nrf24l01_state_s *state);
+static void sm_state_receive_handler(eg_nrf24l01_state_s *state);
+static void sm_state_receiving_handler(eg_nrf24l01_state_s *state);
+static void sm_state_transmit_handler(eg_nrf24l01_state_s *state);
+static void sm_state_transmitting_handler(eg_nrf24l01_state_s *state);
+static void sm_state_powering_off_handler(eg_nrf24l01_state_s *state);
+
+static void spi_write_config_register(eg_nrf24l01_state_s *state);
+
+typedef void (*state_handler)(eg_nrf24l01_state_s *state);
+
+const state_handler state_handlers_lut[NRF_SM_MAX_STATE] = {
+    sm_state_power_off_handler,
+    sm_state_module_startup_handler,
+    sm_state_configure_handler,
+    sm_state_sleep_handler,
+    sm_state_idle_handler,
+    sm_state_ext_int_handler,
+    sm_state_status_reading_handler,
+    sm_state_receive_handler,
+    sm_state_receiving_handler,
+    sm_state_transmit_handler,
+    sm_state_transmitting_handler,
+    sm_state_powering_off_handler};
+
+eg_nrf_error_e eg_nrf24l01_init(eg_nrf24l01_state_s *state, eg_nrf24l01_init_data_s *init_data)
 {
     if (NULL == state || NULL == init_data)
     {
@@ -67,5 +112,126 @@ eg_nrf_error_e eg_nrf24l01_config(eg_nrf24l01_state_s *state, eg_nrf24l01_init_d
         }
     }
 
+    state->config_registers.config.en_crc = 1u;
+
+    state->set_ce_callback = init_data->set_ce_callback;
+    state->set_csn_callback = init_data->set_csn_callback;
+
     return NRF_OK;
+}
+
+void eg_nrf24l01_process(eg_nrf24l01_state_s *state)
+{
+    if (NULL == state)
+    {
+        return;
+    }
+    if (state->sm_state >= NRF_SM_MAX_STATE)
+    {
+        return;
+    }
+    /* Execute state handler */
+    state_handlers_lut[state->sm_state](state);
+}
+
+eg_nrf_error_e eg_nrf24l01_power_on(eg_nrf24l01_state_s *state)
+{
+    if (NULL == state)
+    {
+        return NRF_INVALID_ARGUMENT;
+    }
+
+    state->power_on_request = 1u;
+
+    return NRF_OK;
+}
+
+void eg_nrf24l01_spi_comm_complete(eg_nrf24l01_state_s *state,
+                                   uint8_t rx_len)
+{
+    state->spi_rx_data_len = rx_len;
+    state->spi_data_ready = 1u;
+
+    if (state->set_csn_callback != NULL)
+    {
+        state->set_csn_callback(1u);
+    }
+}
+
+static void sm_state_power_off_handler(eg_nrf24l01_state_s *state)
+{
+    if (1u == state->power_on_request)
+    {
+        state->timestamp = eg_nrf24l01_user_timestamp_get() + POWER_ON_TIME_MS;
+
+        state->config_registers.config.pwr_up = 1u;
+        spi_write_config_register(state);
+
+        state->sm_state = NRF_SM_MODULE_STARTUP;
+    }
+}
+static void sm_state_module_startup_handler(eg_nrf24l01_state_s *state)
+{
+    if (1u == state->spi_data_ready)
+    {
+        uint64_t timestamp = eg_nrf24l01_user_timestamp_get();
+        if (timestamp >= state->timestamp)
+        {
+            state->sm_state = NRF_SM_CONFIGURE;
+        }
+    }
+}
+static void sm_state_configure_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_sleep_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_idle_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_ext_int_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_status_reading_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_receive_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_receiving_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_transmit_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_transmitting_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+static void sm_state_powering_off_handler(eg_nrf24l01_state_s *state)
+{
+    asm("nop");
+}
+
+static void spi_write_config_register(eg_nrf24l01_state_s *state)
+{
+    if (state->set_csn_callback != NULL)
+    {
+        state->set_csn_callback(0u);
+    }
+
+    state->spi_tx_buf[0u] = NRF_CMD_WRITE_REG | NRF_REG_CONFIG;
+    state->spi_tx_buf[1u] = state->config_registers.config.val;
+
+    state->spi_data_ready = 0u;
+    eg_nrf24l01_user_spi_transmit_receive(state, state->spi_tx_buf, 2u, state->spi_rx_buf, 2u);
 }
--- eg_nrf24l01.h
+++ eg_nrf24l01.h
@@ -7,6 +7,7 @@
 #define EG_NRF24L01_MAX_ADDRESS_NO 6u
 
 typedef void (*eg_nrf_rx_callback)(uint8_t *data, uint8_t data_size);
+typedef void (*eg_nrf_set_pin_state_callback)(uint8_t state);
 
 typedef enum
 {
@@ -14,6 +15,7 @@ typedef enum
     NRF_INVALID_ADDRESS_WIDTH = -100,
     NRF_INVALID_ARGUMENT,
     NRF_INVALID_P2_P5_ADDRESS, /**< 4 MSB's of P2-P5 address must be the same as 4 MSB's of P1 address */
+    NRF_INVALID_STATE
 } eg_nrf_error_e;
 
 typedef enum
@@ -34,6 +36,8 @@ typedef struct
         uint8_t auto_ack;                               /**< Enable auto acknowledge */
         eg_nrf_rx_callback rx_callback;                 /**< User function callback to handle incomming data */
     } rx_pipe[EG_NRF24L01_MAX_ADDRESS_NO];              /**< RX addresses */
+    eg_nrf_set_pin_state_callback set_ce_callback;
+    eg_nrf_set_pin_state_callback set_csn_callback;
 } eg_nrf24l01_init_data_s;
 
 typedef struct
@@ -47,6 +51,22 @@ typedef struct
     uint8_t aw : 2;
     uint8_t : 6;
 } eg_nrf24l01_setup_aw_reg_s;
+
+typedef union
+{
+    struct
+    {
+        uint8_t prim_rx : 1;
+        uint8_t pwr_up : 1;
+        uint8_t crco : 1;
+        uint8_t en_crc : 1;
+        uint8_t mask_max_rt : 1;
+        uint8_t mask_max_ds : 1;
+        uint8_t mask_rx_dr : 1;
+        uint8_t res : 1;
+    } __attribute__((packed));
+    uint8_t val;
+} eg_nrf24l01_config_reg_s;
 
 typedef struct
 {
@@ -84,6 +104,24 @@ typedef struct
     };
 } __attribute__((packed)) eg_nrf24l01_en_rxaddr_reg_s;
 
+/**  */
+typedef enum
+{
+    NRF_SM_POWER_OFF = 0,
+    NRF_SM_MODULE_STARTUP,
+    NRF_SM_CONFIGURE,
+    NRF_SM_SLEEP,
+    NRF_SM_IDLE,
+    NRF_SM_EXT_INT,
+    NRF_SM_STATUS_READING,
+    NRF_SM_RECEIVE,
+    NRF_SM_RECEIVING,
+    NRF_SM_TRANSMIT,
+    NRF_SM_TRANSMITTING,
+    NRF_SM_POWERING_OFF,
+    NRF_SM_MAX_STATE,
+} eg_nrf24l01_sm_state_e;
+
 /** NRF24L01 internal state structure */
 typedef struct
 {
@@ -98,11 +136,29 @@ typedef struct
         uint8_t rx_addr_p5;
         eg_nrf24l01_en_rxaddr_reg_s en_rxaddr;
         eg_nrf24l01_en_rxaddr_reg_s en_aa;
+        eg_nrf24l01_config_reg_s config;
     } config_registers;
+
     struct
     {
         eg_nrf_rx_callback rx_callback;
     } rx_pipe[EG_NRF24L01_ADDRESS_MAX_WIDTH];
+    
+    eg_nrf_set_pin_state_callback set_ce_callback;
+    eg_nrf_set_pin_state_callback set_csn_callback;
+
+    /* Flags*/
+    volatile uint8_t power_on_request;
+    volatile uint8_t spi_data_ready;
+
+    /* SPI specific */
+    uint8_t spi_tx_buf[50];
+    uint8_t spi_rx_buf[50];
+    volatile uint8_t spi_rx_data_len;
+
+    /* SM data */
+    uint64_t timestamp;
+    eg_nrf24l01_sm_state_e sm_state;
 } eg_nrf24l01_state_s;
 
 /**
@@ -112,6 +168,49 @@ typedef struct
  * @param init_data pointer to initialization data object
  * @return eg_nrf_error_e error code
  */
-extern eg_nrf_error_e eg_nrf24l01_config(eg_nrf24l01_state_s *state, eg_nrf24l01_init_data_s *init_data);
+extern eg_nrf_error_e eg_nrf24l01_init(eg_nrf24l01_state_s *state, eg_nrf24l01_init_data_s *init_data);
 
+/**
+ * Function to process state machine of given driver instance
+ *
+ * @param state pointer to internal driver state object
+ */
+extern void eg_nrf24l01_process(eg_nrf24l01_state_s *state);
+
+extern eg_nrf_error_e eg_nrf24l01_power_on(eg_nrf24l01_state_s *state);
+
+/**
+ * User function to get timestamp in ms.
+ * @brief User must define it somwhere in own code.
+ *
+ * @return uint64_t current time in milliseconds.
+ */
+extern uint64_t eg_nrf24l01_user_timestamp_get(void);
+
+/**
+ * User function to handle SPI data transmit / receive.
+ * @brief User must define it somwhere in own code.
+ * Function should not exceed rx buf length.
+ *
+ * @param state pointer to internal driver state object
+ * @param tx_buf pointer to tx data buffer
+ * @param tx_len length of data to transmit
+ * @param rx_buf pointer to rx data buffer
+ * @param rx_len expected length of data
+ */
+
+extern void eg_nrf24l01_user_spi_transmit_receive(eg_nrf24l01_state_s *state,
+                                                  uint8_t *tx_buf,
+                                                  uint8_t tx_len,
+                                                  uint8_t *rx_buf,
+                                                  uint8_t rx_len);
+
+/**
+ * Function to handle SPI transmit / receive data complete event
+ *
+ * @param state pointer to internal driver state object given by eg_nrf24l01_user_spi_transmit_receive function
+ * @param rx_len length of data read from SPI
+ */
+extern void eg_nrf24l01_spi_comm_complete(eg_nrf24l01_state_s *state,
+                                          uint8_t rx_len);
 #endif /* _EG_NRF24L01_H_ */
